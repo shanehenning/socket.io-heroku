da1c5b46eb7dd9d4d4db4e6c79e63f01cc6c7066
Shane Henning <shanerhenning@gmail.com>
2016-08-09 14:14:49 -0700

    15

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const server = express()
 
 const io = socketIO(server);
 
+console.log(io);
+
 io.on('connection', (socket)=>{
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
